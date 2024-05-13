abc0335d353702bc2cf8520797abac567d0ef0cb
ayyildiz-efe <ayyildizefe95@gmail.com>
2024-05-13 21:47:02 +0300

    first commit

---
+++ in-memory/src/main/java/org/efeayyildiz/inmemory/controller/PrivateController.java
@@ -0,0 +1,25 @@
+package org.efeayyildiz.inmemory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/private")
+public class PrivateController {
+
+    @GetMapping
+    private String helloWorldPrivate(){
+        return "Hello World! from private endpoint";
+    }
+
+    @GetMapping("/user")
+    private String helloWorldPrivateUser(){
+        return "Hello World! from private endpoint for user";
+    }
+
+    @GetMapping("/admin")
+    private String helloWorldPrivateAdmin(){
+        return "Hello World! from private endpoint for admin";
+    }
+}
